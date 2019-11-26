b65d9f6d01bad0d8c5fb3ea9a097c7059f35e410
Alvani <alpha2_yui@yahoo.com>
2019-11-26 15:45:30 +0700

    implement open csv on example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:csv/csv.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quick_look/flutter_quick_look.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,17 +14,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool writing = false;
+  String path;
+
   @override
   void initState() {
     super.initState();    
   }
 
-  final Map<String, dynamic> anu = {
-    'hello': 'worldstoragepath',
-  };
+  writeCSV() async {
+    if (defaultTargetPlatform  == TargetPlatform.iOS) {      
+      const data = [
+        ['O', 'X', 'X'],
+        ['X', 'O', 'X'],
+        ['X', 'X', 'O']
+      ];
+
+      String csv = const ListToCsvConverter().convert(data);
+      String directory = (await getApplicationDocumentsDirectory()).path;
+      var file = await File('$directory/test.csv').writeAsString(csv);      
+      setState(() {
+        path = file.path;
+        writing = false;  
+      });
+    } else {
+      setState(() {
+        writing = false;  
+      });  
+    }     
+  }
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) { 
+    print(path);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -27,8 +54,19 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child:            
-            FlutterQuickLook(''),          
+            (path == null) ? Text('Please press Open CSV button') : FlutterQuickLook(path),          
         ),
+        persistentFooterButtons: <Widget>[
+          writing ? Text('Writing') : RaisedButton(            
+            onPressed: () {
+              setState(() {
+                writing = true;  
+              });              
+              writeCSV();
+            },
+            child: Text('Open CSV', style: TextStyle(color: Colors.black),),
+          )
+        ],
       ),
     );
   }
